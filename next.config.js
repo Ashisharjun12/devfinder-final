f6649607f1bd1ff6e9c444038ecafeb8d8e997f6
Ashish Raj <ashishrahul748@gmail.com>
2024-10-27 05:39:15 +0530

    deploy test -2

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['www.gravatar.com', 'lh3.googleusercontent.com'],
-    unoptimized: true
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverActions: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        dns: false,
-        fs: false,
-        net: false,
-        tls: false,
-        'mongodb-client-encryption': false
-      };
-    }
-    return config;
-  },
-  output: 'standalone',
 }
 
 module.exports = nextConfig
